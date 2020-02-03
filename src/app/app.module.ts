636cab9316e5756020d43ccadb814f15855db5b5
Matty <matty.fieten@telenet.be>
2020-02-03 15:01:48 +0100

    basis layout zoeken

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,19 @@ import { LiedComponent } from './lied/lied.component';
 import { AboutComponent } from './about/about.component';
 import { ZoekComponent } from './zoek/zoek.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { Routes, RouterModule} from '@angular/router';
+
+const routes: Routes = [
+  {path:"", redirectTo:'zoek', pathMatch:'full'},
+  {path: 'artiest', component: ArtiestComponent},
+  {path: 'concert', component: ConcertComponent},
+  {path: 'lied', component: LiedComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'zoek', component: ZoekComponent},
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +33,9 @@ import { ZoekComponent } from './zoek/zoek.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/zoek/zoek.component.ts
+++ src/app/zoek/zoek.component.ts
@@ -12,4 +12,8 @@ export class ZoekComponent implements OnInit {
   ngOnInit() {
   }
 
+  zoek(keyword) {
+    
+  }
+
 }
