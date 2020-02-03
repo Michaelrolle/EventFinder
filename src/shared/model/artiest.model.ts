dde28496811336b6b57f828dbac45faccb008d87
Matty <matty.fieten@telenet.be>
2020-02-03 14:20:37 +0100

    navbar proberen werkend te maken

---
+++ src/shared/model/artiest.model.ts
@@ -0,0 +1,9 @@
+export class Artiest {
+    constructor (
+        public id: number,
+        public name: string,
+        public image: string,
+        public facebook_page_url: string,
+        public upcoming_event_count: number
+    ){}
+}
---
+++ src/shared/model/concert.model.ts
@@ -0,0 +1,7 @@
+export class Concerten {
+    constructor (
+        public id: number,
+        public venue: string,
+        public offers: string       
+    ){}
+}
